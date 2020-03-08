0010557da4c21623280a242f4da7c713d95e1bad
kipmadden <56366836+kipmadden@users.noreply.github.com>
2020-03-08 17:10:20 -0500

    Copy of group project SeanKDA repository
    Post submission initial copy of SMU Group project : ETL "Aliens"

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ETL-Aliens
-ETL assignment with SMU collaborators Sean Kendrick Del Alcazar, Lance Westlake, Nyarai Nhamoinesu
---
+++ config.py
@@ -0,0 +1,5 @@
+# Enter your API key
+gkey = "AIzaSyATfHYvpoeJbnbT8R-wzSdPLxQpii4gNpY"
+
+# AIzaSyCVVhINcYk6UJHElcROziogaKbIlK8MPLI
+# AIzaSyATfHYvpoeJbnbT8R-wzSdPLxQpii4gNpY
